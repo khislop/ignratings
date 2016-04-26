ae192b19bba2d7ae544f46c418d722a92b0d693b
Kelton J. Hislop <khislop@bb136-22.mines.edu>
2016-04-25 19:38:09 -0600

    Created and populated all tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,20 +1,26 @@
+DROP TABLE IF EXISTS global2 CASCADE;
 DROP TABLE IF EXISTS games CASCADE;
 DROP TABLE IF EXISTS genres CASCADE;
-DROP TABLE IF EXISTS platforms CASCADE;
+--DROP TABLE IF EXISTS platforms CASCADE;
 DROP TABLE IF EXISTS game_genres CASCADE;
-DROP TABLE IF EXISTS game_platforms CASCADE;
+--DROP TABLE IF EXISTS game_platforms CASCADE;
 
+CREATE TABLE global2(
+name text,
+platform text,
+rating text,
+genre text
+);
 CREATE TABLE games(
 id serial PRIMARY KEY,
+platform text,
 title text,
 score text
 );
 CREATE TABLE genres(
 genre text PRIMARY KEY
 );
-CREATE TABLE platforms(
-platform text PRIMARY KEY
-);
+
 CREATE TABLE game_genres(
 game_id integer,
 genre text,
@@ -22,12 +28,13 @@ primary key (game_id, genre),
 foreign key (game_id) references games(id),
 foreign key (genre) references genres(genre)
 );
-CREATE TABLE game_platforms(
-game_id integer,
-platforn text,
-primary key (game_id, platforn),
-foreign key (game_id) references games(id),
-foreign key (platform) references platforms(genre)
-);
 
+INSERT INTO global2 (name, platform, rating, genre) SELECT gi.name, gi.platform, gi.rating, split_part(gi.genre,', ',1) FROM global_ign gi WHERE genre LIKE '%, %';
+INSERT INTO global2 (name, platform, rating, genre) SELECT gi.name, gi.platform, gi.rating, split_part(gi.genre,', ',2) FROM global_ign gi WHERE genre LIKE '%, %';
+INSERT INTO global2 (name, platform, rating, genre) SELECT gi.name, gi.platform, gi.rating, gi.genre FROM global_ign gi WHERE genre NOT LIKE '%, %';
+INSERT INTO games (title, platform, score) SELECT DISTINCT name, platform, rating FROM global2 ORDER BY name;
+INSERT INTO genres SELECT DISTINCT genre FROM global2;
+INSERT INTO game_genres (game_id, genre) SELECT DISTINCT games.id, g2.genre FROM games, global2 g2 WHERE games.title = g2.name;
 
+--SELECT * FROM global_ign WHERE genre LIKE '%, %';
+--SELECT DISTINCT name, AVG(rating) FROM global_ign GROUP BY name ORDER BY name;
--- table_loader.sql
+++ table_loader.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS global_ign CASCADE;
+
 CREATE TABLE global_ign
 (NAME varchar, PLATFORM varchar, RATING double precision, GENRE varchar);
 
-\copy global_ign FROM 'your path to csv here' DELIMITER ',' CSV;
+\copy global_ign FROM '~/csci403/project6/ignratings/global_table.csv' DELIMITER ',' CSV;

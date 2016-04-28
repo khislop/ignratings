296eae6c588a46b9b6ef85024f72bccabceb3a44
Kelton Hislop <khislop@mines.edu>
2016-04-28 16:24:54 -0600

    Fixed a bug and added function stubs for the algorithms

--- RatingPredictor.py
+++ RatingPredictor.py
@@ -76,18 +76,22 @@ db.close()
 
 # Should return a dictionary (map) with each word in the tiltes with it's corisponding score. We can then average the scores of the words in a game title to get it's expected score based on name.
 def weightNames():
-
+    return
+    
 # Should return a dictionary with each genre and it's corisponding average score.
 def weightGenres():
-
+    return
+    
 # Should return a dictionary with each platform and it's corisponding average score.
 def weightPlatforms():
-
+    return
+    
 # Should return a dictionary with the keys being a tuple of (genre, platform) and values being the score. This gives us a much better prediction then just averaging the genres and platforms.
 # It is a little weird to have a tuple as a key but Python suports this and it will make lookup really easy. An example of thie I got to work:
 # weightPG[('Action', 'Wii')] = "Woot"
 def weightPlatGenre():
-
+    return
+    
     
 ############################################################################################
     
